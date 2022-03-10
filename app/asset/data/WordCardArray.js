4d26f0ce22ee389b6eb2c198ff1a1d63d82833fc
coqoa <choibm0208@gmail.com>
2022-03-10 14:05:03 +0900

    220310 asset 추가

---
+++ app/asset/data/WordCardArray.js
@@ -0,0 +1,97 @@
+import { colors } from "../../component/color"
+
+
+export const WordCardArray = [
+    {
+        "id" : 0, 
+        "image" : require("../images/Lion1.png"), 
+        "bgColor" : colors.ORANGE, 
+        "name" : "사자",
+        "check" : true
+    }, 
+    {
+        "id" : 1, 
+        "image" : require("../images/Speaker.png"), 
+        "bgColor" : colors.LIGHTBLUE, 
+        "name" : "말",
+        "check" : false
+    }, 
+    {
+        "id" : 2, 
+        "image" : require("../images/Star.png"),
+        "bgColor" : colors.TOMATO,  
+        "name" : "강아지",
+        "check" : false
+    }, 
+    {
+        "id" : 3, 
+        "image" : require("../images/Random.png"), 
+        "bgColor" : colors.BEIGE, 
+        "name" : "고양이",
+        "check" : false
+    }, 
+    {
+        "id" : 4, 
+        "image" : require("../images/Heart1.png"), 
+        "bgColor" : colors.NAVY, 
+        "name" : "쥐",
+        "check" : false
+    }, 
+    {
+        "id" : 5, 
+        "image" : require("../images/Heart2.png"), 
+        "bgColor" : colors.PASTELBLUE, 
+        "name" : "소",
+        "check" : false
+    },
+
+    {
+        "id" : 6, 
+        "image" : require("../images/Heart3.png"), 
+        "bgColor" : colors.REALLIGHTGRAY, 
+        "name" : "6",
+        "check" : false
+    }, 
+    {
+        "id" : 7, 
+        "image" : require("../images/Lion2.png"), 
+        "bgColor" : colors.LIGHTGRAY, 
+        "name" : "7",
+        "check" : false
+    }, 
+    {
+        "id" : 8, 
+        "image" : require("../images/LeftArrow.png"), 
+        "bgColor" : colors.ORANGE, 
+        "name" : "8",
+        "check" : false
+    }, 
+    {
+        "id" : 9, 
+        "image" : require("../images/RightArrow.png"), 
+        "bgColor" : colors.BEIGE, 
+        "name" : "9",
+        "check" : false
+    }, 
+
+
+
+
+
+    {
+        "id" : 9999, 
+        "image" : require("../images/RightArrow.png"), 
+        "bgColor" : colors.BEIGE, 
+        "name" : "끝",
+        "check" : false
+    }, 
+    {
+        "id" : 10000, 
+        "image" :require("../images/RightArrow.png"), 
+        "bgColor" : " ", 
+        "name" : " ",
+        "check" : false
+    }
+]
+
+

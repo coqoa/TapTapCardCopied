fdb05b35c88e552a01c9ddfe292b6fe8a159cfc6
coqoa <choibm0208@gmail.com>
2022-04-07 18:53:56 +0900

    220407 lottie files 적용 / clearModal 컴포넌트화

---
+++ app/component/AnimalAnimation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Lottie from 'lottie-react-native';
+import { AnimalCardArray } from "../asset/data/AnimalCardArray";
+
+const AnimalAnimation = (props) => {
+    return <Lottie source={AnimalCardArray[props.id].image} style={{width: AnimalCardArray[props.id].size}} autoPlay loop />;
+};
+
+export default AnimalAnimation;
---
+++ app/component/ClearModal.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Audio } from 'expo-av';
+import Lottie from 'lottie-react-native';
+import styled from "styled-components"
+import { colors } from "./Color";
+
+const ClearModal = (props) => {
+
+    const ClearModal = styled.View`
+    position: absolute;
+    
+    width: 300px;
+    height: 300px;
+    border-radius: 15px;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
+    `
+    const RepeatLevel = styled.TouchableOpacity`
+        width: 200px;
+        height: 60px;
+        border-radius: 15px;
+        margin: 5px;
+        align-items: center;
+        justify-content: center;
+        background-color: rgba(255,255,255,0.8);
+    `
+    const NextLevel = styled(RepeatLevel)`
+    `
+    const RepeatLevelText = styled.Text`
+        font-size: 30px;
+        color: white;
+        color: ${colors.DARKNAVY};
+        font-family: 'SDChild';
+    `
+    const NextLevelText = styled(RepeatLevelText)`
+        color: ${colors.CUSTOMPINK};
+    `
+
+    const ClickSound = async() => {
+        const sound = new Audio.Sound();
+        try {    
+            await sound.loadAsync(require("../asset/audio/btnClickSound.mp3"));
+            await sound.playAsync();
+        } catch (error) {
+        }
+    }
+
+    return (
+        <>
+        <Lottie source={require("../asset/lottie/last1.json")} style={{position:"absolute", width: 450}} autoPlay loop />
+        <Lottie source={require("../asset/lottie/last.json")} style={{width: 500}} autoPlay loop />
+        <ClearModal>
+            <RepeatLevel
+                onPressIn={() => ClickSound()}
+                onPressOut={() => {props.restartLevelBtn()}}
+            >
+                <RepeatLevelText>Again !</RepeatLevelText>
+            </RepeatLevel>
+
+            { props.level == "word1LV" && (
+                <NextLevel 
+                    onPressIn={()=> ClickSound()}
+                    onPressOut={()=> props.nextLevelBtn(props.level)}
+                >
+                    {props.type == "AnimalKOR" && (<NextLevelText>다음레벨 도전 !</NextLevelText>)}
+                    {props.type == "AnimalENG" && (<NextLevelText>Next Level !</NextLevelText>)}
+                </NextLevel>
+            )}
+            { props.level == "word2LV" && (
+                <NextLevel 
+                    onPressIn={()=> ClickSound()}
+                    onPressOut={()=> props.nextLevelBtn(props.level)}
+                >
+                    {props.type == "AnimalKOR" && (<NextLevelText>다음레벨 도전 !</NextLevelText>)}
+                    {props.type == "AnimalENG" && (<NextLevelText>Next Level !</NextLevelText>)}
+                </NextLevel>
+            )}
+            {props.type=="Number" &&  props.level !== "All" && props.level !=="91~100" && (
+                <NextLevel 
+                onPressIn={()=> ClickSound()}
+                onPressOut={()=> props.nextLevelBtn(props.level)}>
+                    <NextLevelText>다음레벨 도전 !</NextLevelText>
+                </NextLevel>
+            )}
+        </ClearModal>
+        </>
+    )
+};
+export default ClearModal;

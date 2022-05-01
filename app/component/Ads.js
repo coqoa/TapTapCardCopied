a5a38dd4edb6ca3fd9b085605045a82b0c1e4a46
coqoa <choibm0208@gmail.com>
2022-05-01 22:05:58 +0900

    220501 앱빌드오류확인중(white screen관련)

--- app/component/Ads.js
+++ app/component/Ads.js
@@ -8,33 +8,26 @@ const BannerShell = styled.View`
     width:100%;
     height: 100%;
 `
-// {console.log("OS = ",Platform.OS, "test id = ",TestIds)}
-
 export const BannerAds = () =>{
     const [showBanner, setShowBanner] = useState(false);
-    // {console.log(Platform.OS)}
     useEffect(()=>{
         mobileAds()
             .setRequestConfiguration({
                 // Update all future requests suitable for parental guidance
                 maxAdContentRating: MaxAdContentRating.PG,
-
                 // Indicates that you want your content treated as child-directed for purposes of COPPA.
                 tagForChildDirectedTreatment: true,
-
                 // Indicates that you want the ad request to be handled in a
                 // manner suitable for users under the age of consent.
                 tagForUnderAgeOfConsent: true,
-
                 // An array of test device IDs to allow.
                 testDeviceIdentifiers: ['EMULATOR'],
             })
             .then(() => {
-                setShowBanner(true)
                 // Request config successfully set!
+                setShowBanner(true)
             });
     },[])
-    
     // console.log('TestIds = ',TestIds)
     // console.log('BannerAdSize = ',BannerAdSize)
     return(
@@ -53,4 +46,3 @@ export const BannerAds = () =>{
     </>
     )
 }
-// export default BannerAd;

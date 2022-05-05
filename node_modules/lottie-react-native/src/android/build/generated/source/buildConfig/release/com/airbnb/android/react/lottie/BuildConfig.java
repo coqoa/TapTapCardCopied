0bbcafa20471e6fa7898db93465e0cd0713fd6eb
coqoa <choibm0208@gmail.com>
2022-05-06 01:13:48 +0900

    220506 apk파일 만들기

---
+++ node_modules/lottie-react-native/src/android/build/generated/source/buildConfig/release/com/airbnb/android/react/lottie/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.airbnb.android.react.lottie;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.airbnb.android.react.lottie";
+  public static final String BUILD_TYPE = "release";
+}

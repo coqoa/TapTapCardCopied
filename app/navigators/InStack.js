1eb7416a5650e0ed306f184b7c8922fcfda9274f
coqoa <choibm0208@gmail.com>
2022-05-11 00:47:38 +0900

    20220511 App.js, 네비게이터 코드정리 및 주석처리

--- app/navigators/InStack.js
+++ app/navigators/InStack.js
@@ -1,11 +1,9 @@
 import React from "react";
 import {createNativeStackNavigator} from "@react-navigation/native-stack" 
-
 import MenuStack from "./MenuStack";
 
 const NativeStack = createNativeStackNavigator();
 
-// 스택네비게이터 (Login, Signup, MenuStack을 Screen으로 가진다)
 const Stack = () => {
     return(
     <NativeStack.Navigator screenOptions={{headerShown: false}} >

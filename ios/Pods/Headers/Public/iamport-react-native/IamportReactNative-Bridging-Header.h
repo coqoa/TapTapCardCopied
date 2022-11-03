f6aad9d091eb681745d6777d01436d542dd982a9
Choi ByeongMin <choibm0208@gmail.com>
2022-11-03 13:55:58 +0900

    Delete ios directory

--- ios/Pods/Headers/Public/iamport-react-native/IamportReactNative-Bridging-Header.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/iamport-react-native/ios/IamportReactNative-Bridging-Header.h
--- ios/Pods/Target Support Files/iamport-react-native/iamport-react-native-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_iamport_react_native : NSObject
-@end
-@implementation PodsDummy_iamport_react_native
-@end
